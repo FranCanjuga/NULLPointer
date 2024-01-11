7661d828b1c2020e61ca9d7a62c6f8c3b5dfbcd7
Fran Canjuga <fran.canjuga@fer.hr>
2024-01-11 16:12:09 +0100

    Dodani cross i buttoni za povratak

--- doniranje_krvi_front/src/components/user.js
+++ doniranje_krvi_front/src/components/user.js
@@ -22,44 +22,43 @@ const User = () => {
               },
           });
           setUserData(response.data);
-        } else {
+        } 
+        else if (roles.includes("user")) {
+          console.log(decodedToken.sub)
+          const userParam = `${decodedToken.sub}`;
 
-          
           const response = await axios.get(
             `${baseURL}/user/profile`,
             {
-              params: {
-                user: decodedToken.sub
-              },
               headers: {
-                  Authorization: `Bearer ${token}`,
-              }
-            },
+                Authorization: `Bearer ${token}`,
+              },
+              body: {
+                user: userParam,
+              },
+            }
           );
-        
-        console.log(response.data)
-        setDonorData(response.data);
-        }
-
+            
+            console.log(response.data)
+            setDonorData(response.data);
+          }
       } catch (error) {
         console.error("Error fetching user data:", error);
       }
     };
   
     fetchData();
-  }, [token, roles]);
+  }, [token, roles, decodedToken.sub]);
 
 
   const approveUser = (username) => {
-
-
-    axios.post(`${baseURL}/admin/approveDonor`,{username : username},{
+    const response = axios.get(`${baseURL}/admin/approveDonor`,{
       headers: {
-        Authorization: `Bearer ${token}`,
-    },
-  })
+          Authorization: `Bearer ${token}`,
+        },
+    })
       .then(() => {
-        console.log(`User ${username} approved successfully`);
+        console.log(response.data);
         setUserData((prevUserData) => prevUserData.filter((user) => user.username !== username));
       })
       .catch((error) => {
@@ -69,54 +68,104 @@ const User = () => {
   };
 
   const rejectUser = (username) => {
-    axios.post(`${baseURL}/admin/rejectDonor`, { username })
+    const response = axios.post(`${baseURL}/admin/rejectDonor`,{
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body :{
+        username : username
+      },
+    })
       .then(() => {
-        console.log("User rejected successfully");
+        console.log(response.data);
+        setUserData((prevUserData) => prevUserData.filter((user) => user.username !== username));
       })
       .catch((error) => {
         console.error("Error rejecting user:", error);
       });
   };
 
+  const addAppointment =() => {
+    const response =  axios.post(`${baseURL}/cross/addAppointment`,{
+      headers: {
+          Authorization: `Bearer ${token}`,
+        }
+
+    }).then(()=>{
+      console.log(response.data)
+      
+    }).catch((error) => {
+        
+      console.error(error);
+    });
+  };
+
+  const activeApp = async() => {
+
+  };
+
+  const registeredForApp =async() => {
+
+  };
+
+  const povratak =() =>{
+    window.location.href = '/';
+  }
 
   return (
     <div>
-      
-
-      {roles.includes("admin") ? (
-        <div>
-            <h1>Pozdrav admin</h1>
-            <br></br>
-            <h1>Svi neverificirani korisnici</h1>
-            <br></br>
-            <ul>
-            {userData.map((user) => (
-              <li key={user.donorID}>
-                <p>Username: {user.username}</p>
-                <p>Blood Type: {user.bloodType}</p>
-                <p>Donor ID: {user.donorID}</p>
-                <p>Town: {user.town}</p>
-                <button onClick={() => rejectUser(user.username)}>Reject</button>{' '}
-                <button onClick={() => approveUser(user.username)}>Approve</button>
-              </li>
-            ))}
-            </ul>
-        
-        </div>
-      ) : (
-        <div>
-          <h2>Vaši podaci</h2>
-          <p>Korisničko ime: {donorData.username}</p>
-          <p>Ime: {donorData.firstName}</p>
-          <p>Prezime: {donorData.lastName}</p>
-          <p>Broj mobitela: {donorData.phoneNumber}</p>
-          <p>Datum rođenja: {donorData.dateOfBirth}</p>
-          <p>Mjesto: {donorData.city}</p>
-          <p>Adresa: {donorData.address}</p>
-          <p>Krvna grupa: {donorData.bloodType}</p>
-        </div>
-      )}
+  {roles.includes("admin") ? (
+    <div>
+      <h1>Pozdrav admin</h1>
+      <br />
+      <button onClick={() => povratak()}>Vrati se</button>
+      <br></br>
+      <h1>Svi neverificirani korisnici</h1>
+      <br />
+      <ul>
+        {userData.map((user) => (
+          <li key={user.donorID}>
+            <p>Username: {user.username}</p>
+            <p>Blood Type: {user.bloodType}</p>
+            <p>Donor ID: {user.donorID}</p>
+            <p>Town: {user.town}</p>
+            <button onClick={() => rejectUser(user.username)}>Reject</button>{' '}
+            <button onClick={() => approveUser(user.username)}>Approve</button>
+          </li>
+        ))}
+      </ul>
     </div>
+  ) : roles.includes("user") ? (
+    <div>
+      <h2>Vaši podaci</h2>
+      <p>Korisničko ime: {decodedToken.username}</p>
+      <p>Ime: {decodedToken.firstName}</p>
+      <p>Prezime: {decodedToken.lastName}</p>
+      <p>Broj mobitela: {decodedToken.phoneNumber}</p>
+      <p>Datum rođenja: {decodedToken.dateOfBirth}</p>
+      <p>Mjesto: {decodedToken.city}</p>
+      <p>Adresa: {decodedToken.address}</p>
+      <p>Krvna grupa: {decodedToken.bloodType}</p>
+    </div>
+  ) : roles.includes("institution") ? (
+    <div>
+      {/* Render content for institution role */}
+      {/* Add your institution-specific content here */}
+    </div>
+  ) : (
+    <div>
+      <h1>Pozdrav Crveni križ</h1>
+      <br></br>
+      <button onClick={() => addAppointment()}>Dodaj sastanak donacija</button>
+      <br></br>
+      <button onClick={() => activeApp()}>Aktivni sastanci donacija</button>
+      <br></br>
+      <button onClick={() => registeredForApp()}>Registrirani za sastanke</button>
+      <br></br>
+
+    </div>
+  )}
+</div>
   );
 }
 

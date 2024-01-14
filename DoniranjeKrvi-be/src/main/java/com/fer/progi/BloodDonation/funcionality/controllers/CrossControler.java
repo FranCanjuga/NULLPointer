769b233b5489b163104ff174125a3cba0bf6bac9
Fran Canjuga <fran.canjuga@fer.hr>
2024-01-14 02:04:27 +0100

    Cross i admin, contact i user do kraja

--- DoniranjeKrvi-be/src/main/java/com/fer/progi/BloodDonation/funcionality/controllers/CrossControler.java
+++ DoniranjeKrvi-be/src/main/java/com/fer/progi/BloodDonation/funcionality/controllers/CrossControler.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 @RequestMapping("/cross")
 @PreAuthorize("hasRole('cross')")
+@CrossOrigin("*")
 public class CrossControler {
 
     @Autowired

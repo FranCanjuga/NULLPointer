156af0ef38d064eaf3b9455d6ca23211dee3093b
ToniGrgurevic <toni.grgurevic@fer.hr>
2024-01-15 12:07:16 +0100

    dodavanje priznanja u DonorDto (String)

---
+++ DoniranjeKrvi-be/src/main/java/com/fer/progi/BloodDonation/funcionality/repositorys/PriznanjaDonoraRepository.java
@@ -0,0 +1,7 @@
+package com.fer.progi.BloodDonation.funcionality.repositorys;
+
+import com.fer.progi.BloodDonation.funcionality.models.PriznanjaDonora;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PriznanjaDonoraRepository extends JpaRepository<PriznanjaDonora, Long> {
+}

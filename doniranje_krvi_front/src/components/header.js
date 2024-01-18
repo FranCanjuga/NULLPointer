c1807944b8221bedf3c84fd25366bdf2c5829703
OliverOzvacic <oo54364@fer.hr>
2024-01-18 12:18:09 +0100

    Maknut scroll, ljepši header, mapa 100vh

--- doniranje_krvi_front/src/components/header.js
+++ doniranje_krvi_front/src/components/header.js
@@ -30,7 +30,10 @@ const Header = () => {
         
         <body className="head">
             <nav>
-                <img src="/images/red_drop.png" className="logo"></img>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill-opacity="0.8" d="M0,192L120,176C240,160,480,128,720,144C960,160,1200,224,1320,256L1440,288L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"></path></svg>
+            <svg id="svg2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill-opacity="0.8" d="M0,192L120,176C240,160,480,128,720,144C960,160,1200,224,1320,256L1440,288L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"></path></svg>
+                
+               <img src="/images/red_drop.png" className="logo"></img>
                 <ul>
                     <li><a href="#">Features</a></li>
                     <li><a href="/contact">Contact</a></li>
